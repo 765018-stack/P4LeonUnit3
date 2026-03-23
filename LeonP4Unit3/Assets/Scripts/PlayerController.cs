3541f7d697355bbd4acfee7e9abd2975653ce53f
765018-stack <765018@student.dusd.net>
2026-03-23 11:58:13 -0700

    start of my project

---
+++ LeonP4Unit3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb:
+    // Start is called once before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
